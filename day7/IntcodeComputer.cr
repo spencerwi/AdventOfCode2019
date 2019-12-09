1f24c8d84f608948de21fb492b16668abb2ea31c
Spencer Williams <spencerwi@gmail.com>
2019-12-09 15:01:09 -0500

    Add Day 7 part 2 solution

--- day7/IntcodeComputer.cr
+++ day7/IntcodeComputer.cr
@@ -1,4 +1,5 @@
 DEBUG = false
+
 enum ParameterMode
   Position = 0,
   Immediate = 1
@@ -17,13 +18,20 @@ enum Opcode
 end
 
 class IntcodeComputer
+  @program : Array(Int64)
   @instruction_pointer : Int64 
-  @printed_values : Array(Int64)
-  getter program
+  @most_recent_output : Int64?
+  @has_finished : Bool
 
-  def initialize(@program : Array(Int64), @input_queue : Array(Int64))
+  getter program
+  getter has_finished
+  getter most_recent_output
+
+  def initialize(program : Array(Int64), @input_queue : Array(Int64))
+    @program = program.clone
     @instruction_pointer = 0
-    @printed_values = [] of Int64
+    @most_recent_output = nil
+    @has_finished = false
   end
 
   def run : Int64
@@ -31,11 +39,11 @@ class IntcodeComputer
       instruction = @program[@instruction_pointer]
       current_opcode, param_modes = parse_instruction(instruction)
       next_jump_amount = 0
-      if DEBUG
-      puts "Excuting #{instruction} (#{current_opcode}, #{param_modes})"
-      end 
+      debug_log "Executing #{instruction} (#{current_opcode}, #{param_modes})"
       case current_opcode
-      when Opcode::Quit then return @printed_values.last
+      when Opcode::Quit then 
+        @has_finished = true
+        return @most_recent_output.not_nil!
       when Opcode::Add then 
         add(param_modes)
         next_jump_amount = 4
@@ -62,14 +70,13 @@ class IntcodeComputer
         next_jump_amount = 4
       end
       @instruction_pointer += next_jump_amount
-      if DEBUG
-      puts "IP: #{@instruction_pointer}, Program: #{@program}"
-      end
+      debug_log "IP: #{@instruction_pointer}, Program: #{@program}"
     end
-    return @printed_values.last
+    @has_finished = true
+    return @most_recent_output.not_nil!
   end
 
-  private def parse_instruction(instruction : Int64) : Tuple(Opcode, Array(ParameterMode))
+  protected def parse_instruction(instruction : Int64) : Tuple(Opcode, Array(ParameterMode))
     modes_as_int, opcode_as_int = instruction.divmod(100)
     opcode : Opcode = Opcode.new(opcode_as_int.to_i)
     modes_digits = modes_as_int.to_s.rjust(3, '0').chars.reverse.map(&.to_i)
@@ -84,43 +91,39 @@ class IntcodeComputer
        else
          modes_digits.map{|it| ParameterMode.new(it)}
        end
-    if DEBUG
-    puts [opcode, param_modes]
-    end
+    debug_log [opcode, param_modes]
     return {opcode, param_modes}
   end
 
-  private def add(param_modes : Array(ParameterMode))
+  protected def add(param_modes : Array(ParameterMode))
     binary_op(param_modes) {|a, b| a + b}
   end
 
-  private def multiply(param_modes : Array(ParameterMode))
+  protected def multiply(param_modes : Array(ParameterMode))
     binary_op(param_modes) {|a, b| a * b}
   end
 
-  private def do_input
+  protected def do_input
     write_result(1, @input_queue.pop)
   end
 
-  private def do_output(param_mode : ParameterMode)
+  protected def do_output(param_mode : ParameterMode)
     result = read_param(param_mode, 1)
-    @printed_values << result
-    if DEBUG
-      puts result
-    end
+    @most_recent_output = result
+    debug_log "output: #{result}"
   end
 
   # Returns a Bool indicating whether a jump happened
-  private def jump_if_true(param_modes : Array(ParameterMode)) : Bool
+  protected def jump_if_true(param_modes : Array(ParameterMode)) : Bool
     jump_if(param_modes) {|a, _| !(a.zero?) }
   end
 
   # Returns a Bool indicating whether a jump happened
-  private def jump_if_false(param_modes : Array(ParameterMode)) : Bool
+  protected def jump_if_false(param_modes : Array(ParameterMode)) : Bool
     jump_if(param_modes) {|a, _| a.zero? }
   end
 
-  private def less_than(param_modes : Array(ParameterMode))
+  protected def less_than(param_modes : Array(ParameterMode))
     binary_op(param_modes) do |a, b|
       if a < b
         1_i64
@@ -130,7 +133,7 @@ class IntcodeComputer
     end
   end
 
-  private def equal_to(param_modes : Array(ParameterMode))
+  protected def equal_to(param_modes : Array(ParameterMode))
     binary_op(param_modes) do |a, b|
       if a == b 
         1_i64 
@@ -142,38 +145,34 @@ class IntcodeComputer
 
   # Convenience method for operating on the values of two memory locations 
   # (yielding to a block) and storing the result in another memory location.
-  private def binary_op(param_modes : Array(ParameterMode), &block : (Int64, Int64) -> Int64)
+  protected def binary_op(param_modes : Array(ParameterMode), &block : (Int64, Int64) -> Int64)
     operands = param_modes.first(2).map_with_index do |mode, idx| 
       read_param(mode, idx + 1)
     end
 
     result = (yield operands[0], operands[1])
 
-    if DEBUG
-    puts "  operands: #{operands}, result: #{result}"
-    end
+    debug_log "  operands: #{operands}, result: #{result}"
 
     write_result(3, result)
   end
 
   # Convenience method for grabbing two values, doing a boolean test, and then jumping if needed
-  private def jump_if(param_modes : Array(ParameterMode), &block : (Int64, Int64) -> Bool)
+  protected def jump_if(param_modes : Array(ParameterMode), &block : (Int64, Int64) -> Bool)
     operands = param_modes.first(2).map_with_index do |mode, idx| 
       read_param(mode, idx + 1)
     end
 
     result = (yield operands[0], operands[1])
 
-    if DEBUG
-    puts "  operands: #{operands}, result: #{result}"
-    end
+    debug_log "  operands: #{operands}, result: #{result}"
 
     @instruction_pointer = operands[1] if result
     return result
   end
 
   # Convenience method for reading a parameter based on a given param mode
-  private def read_param(param_mode : ParameterMode, param_number : Int32)
+  protected def read_param(param_mode : ParameterMode, param_number : Int32)
     case param_mode
     when ParameterMode::Position then
       location = @program[@instruction_pointer + param_number]
@@ -184,11 +183,56 @@ class IntcodeComputer
     end
   end
 
-  private def write_result(param_number : Int32, value : Int64)
+  protected def write_result(param_number : Int32, value : Int64)
     destination = @program[@instruction_pointer + param_number]
-    if DEBUG
-    puts "Writing #{value} to location #{destination}"
-    end
+    debug_log "Writing #{value} to location #{destination}"
     @program[destination] = value
   end
+
+  protected def debug_log(msg)
+    if DEBUG
+      puts msg
+    end
+  end
+end
+
+class NetworkedIntcodeComputer < IntcodeComputer
+  @input_channel : Channel(Int64)
+  @output_channel : Channel(Int64)
+
+  getter name
+  property input_channel, output_channel
+
+  def initialize(@name : Char, @program : Array(Int64))
+    super(@program, [] of Int64)
+    @input_channel = Channel(Int64).new
+    @output_channel = Channel(Int64).new
+  end
+
+  def run
+    spawn do
+      super
+    end
+  end
+
+  protected def do_input
+    debug_log "Waiting for input, last output was #{@most_recent_output}"
+    result = @input_channel.receive
+    write_result(1, result)
+    debug_log "Received #{result}"
+  end
+
+  protected def do_output(param_mode : ParameterMode)
+    result = read_param(param_mode, 1)
+    @most_recent_output = result
+    debug_log "Sending: #{result}"
+    @output_channel.send(result)
+  end
+
+  protected def debug_log(msg)
+    if DEBUG
+      puts "#{@name}: #{msg}"
+    end
+  end
+
 end
--- day7/day7.cr
+++ day7/day7.cr
@@ -10,25 +10,78 @@ class Day7
   # Problem statement: find the largest output value from a computer, based on 
   # a set of input settings to a series of amplifiers. 
   def part1
-    [0_i64,1_i64,2_i64,3_i64,4_i64].permutations(5).max_of do |amplifier_settings|
+    (0_i64..4_i64).to_a.permutations(5).max_of do |amplifier_settings|
       previous_amp_result = 0_i64
       ('A'..'E').each_with_index do |amp, idx|
         # array.pop in Crystal pops from the right, so we put our "input queue" in what would appear to be reverse order.
         input_queue = [previous_amp_result, amplifier_settings[idx.to_i64]]
-        computer = IntcodeComputer.new(@input_program.clone, input_queue)
+        computer = IntcodeComputer.new(@input_program, input_queue)
         previous_amp_result = computer.run
       end
       previous_amp_result
     end
   end
 
+  # Problem statement: use networked amplifiers (A's output goes to B, B's output
+  # goes to C, etc) in a loop (so E -> A), and test permutations of digits 
+  # between 5 and 9 (inclusive), to find the largest possible output from E.
   def part2
-    # TODO: this!
+    (5_i64..9_i64).to_a.permutations(5).max_of do |amplifier_settings|
+      # Create all our amp computers
+      computers_by_name = ('A'..'E').to_h do |name|
+        computer = NetworkedIntcodeComputer.new(name, @input_program)
+        {name, computer}
+      end
+
+      # Then chain together their input channels and feed in the initial inputs
+      computers_by_name.each do |name, computer| 
+        if name == 'E'
+          computer.output_channel = (computers_by_name['A']).input_channel
+        else
+          computer.output_channel = computers_by_name[name + 1].input_channel # 'A' + 1 == 'B'
+        end
+
+
+        amplifier_input_idx = ('A'..'E').index(name).not_nil!
+
+        # Sending initial inputs must be done async, otherwise the main thread will block.
+        spawn do
+          computer.input_channel.send(amplifier_settings[amplifier_input_idx])
+        end
+      end
+
+      #computers_by_name.each_value do |amp|
+      #  puts "#{amp.name}, in: #{amp.input_channel}, out: #{amp.output_channel}"
+      #end
+
+      # Kick them all off concurrently for their first round
+      computers_by_name.each_value do |amp|
+        amp.run
+      end
+
+      # Block the main thread until the first round finishes 
+      Fiber.yield
+
+      # Amplifier A gets 0 as its next input
+      spawn do
+        computers_by_name['A'].input_channel.send(0_i64)
+      end
+
+      # Block the main thread until _that_ finishes
+      Fiber.yield
+
+      # Now just run them all until one halts
+      until computers_by_name.values.any?(&.has_finished)
+        Fiber.yield
+      end
+
+      computers_by_name['E'].most_recent_output.not_nil!
+    end
   end
 end
 
 unless PROGRAM_NAME.includes?("crystal-run-spec")
   day7 = Day7.new(File.read("input.txt").split(",").map(&.to_i64))
   puts "Part 1: #{day7.part1}"
-  #puts "Part 2: #{day7.part2}"
+  puts "Part 2: #{day7.part2}"
 end
